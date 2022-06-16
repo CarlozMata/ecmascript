db8d50c4660c2ece96cf9b2eff31e39c8572e39c
Carlos Alberto López Mata <mata66666666666@gmail.com>
2022-06-16 18:50:00 -0500

    update

---
+++ src/es6/index.js
@@ -0,0 +1,131 @@
+function newFunction(name, age, country) {
+  var name = name || 'oscar';
+  var age = age || 32;
+  var country = country || 'MX';
+  console.log(name, age, country);
+}
+
+//ECMASCRIPT 6
+function newFunction2(name = 'oscar', age = 32, country='MX') {
+  console.log(name, age, country);
+};
+
+newFunction2();
+newFunction2('Ricardo', '23', 'CO');
+
+//////////////////////////////////////////////////////
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+let lorem = "Loremipsu quiero escribir una frase epica. \n"
++ "otra frase epica que necesitamos";
+
+//ECMASCRIPT 6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+//////////////////////////////////////////////////////
+
+let person = {
+  'name' : 'oscar',
+  'age' : 32, 
+  'country': 'MX'
+}
+console.log(person.name, person.age);
+//ECMASCRIPT 6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+//////////////////////////////////////////////////////
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+//////////////////////////////////////////////////////
+
+{
+  var globalVar = "Global Var";
+}
+{
+  let globalLet = "Global Let";
+  console.log(globalLet);
+}
+console.log(globalVar);
+
+//////////////////////////////////////////////////////
+
+var a = 'b';
+a = 'a';
+console.log(a);//El resultado es "a"
+
+const a = 'b';
+a = 'a';
+console.log(a);//El resultado es "b"
+
+///////////////////////////////////////////////////////
+
+let name = 'oscar';
+let age = 32;
+
+obj = { name: name, age: age };
+
+obj2 = { name, age };
+console.log(obj2);
+
+const names = [
+  {name: 'Oscar', age:32},
+  {name: 'Yesica', age:27}
+]
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+});
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {
+  ...
+}
+
+const listOfNames4 = name => {
+  ...
+}
+
+const square = num => num * num;
+
+///////////////////////////////////////////////////////
+
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!')
+    } else {
+      reject('Ups');
+    }
+  });
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .then(() => console.log('Hola'))
+  .catch(error => console.log(error));
+///////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////
